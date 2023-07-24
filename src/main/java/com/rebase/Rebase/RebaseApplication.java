f5c76b2200fb14019637fb15126860c6e11db5be
Avinash <avinash.nichat@neosoftmail.com>
2023-07-24 15:39:14 +0530

    feature A

--- src/main/java/com/rebase/Rebase/RebaseApplication.java
+++ src/main/java/com/rebase/Rebase/RebaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RebaseApplication {
 
 	public static void main(String[] args) {
+		System.out.println("feature A");
 		System.out.println("master");
 		System.out.println("master A");
 		SpringApplication.run(RebaseApplication.class, args);
