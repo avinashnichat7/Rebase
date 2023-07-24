4e52039d80be3fca58cf6ec3fc852c11219193fc
Avinash <avinash.nichat@neosoftmail.com>
2023-07-24 15:39:32 +0530

    feature B

--- src/main/java/com/rebase/Rebase/RebaseApplication.java
+++ src/main/java/com/rebase/Rebase/RebaseApplication.java
@@ -8,6 +8,7 @@ public class RebaseApplication {
 
 	public static void main(String[] args) {
 		System.out.println("feature A");
+		System.out.println("feature B");
 		System.out.println("master");
 		System.out.println("master A");
 		SpringApplication.run(RebaseApplication.class, args);
