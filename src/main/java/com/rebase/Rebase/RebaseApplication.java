f906aa5b8d1f51a567ebab2788f6e4d4f132c744
Avinash <avinash.nichat@neosoftmail.com>
2023-07-24 15:40:32 +0530

    master A

--- src/main/java/com/rebase/Rebase/RebaseApplication.java
+++ src/main/java/com/rebase/Rebase/RebaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RebaseApplication {
 
 	public static void main(String[] args) {
+		System.out.println("master");
 		SpringApplication.run(RebaseApplication.class, args);
 	}
 
