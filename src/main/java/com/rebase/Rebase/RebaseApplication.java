47b52fdb329794614ab66526157f2c72fb18daf4
Avinash <avinash.nichat@neosoftmail.com>
2023-07-24 15:36:28 +0530

    master

---
+++ src/main/java/com/rebase/Rebase/RebaseApplication.java
@@ -0,0 +1,13 @@
+package com.rebase.Rebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebase/Rebase/RebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebase.Rebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
